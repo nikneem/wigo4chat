c8eaa0a929b83ffec11d4af0847b31798ac4e7e6
Eduard Keilholz <e.keilholz@4dotnet.nl>
2025-05-15 12:36:34 +0200

    Changed config

--- src/Aspire/Wigo4it.Chat.Aspire/Wigo4it.Chat.Aspire.AppHost/Program.cs
+++ src/Aspire/Wigo4it.Chat.Aspire/Wigo4it.Chat.Aspire.AppHost/Program.cs
@@ -1,9 +1,16 @@
+using System.Collections.Immutable;
+using CommunityToolkit.Aspire.Hosting.Dapr;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-
+builder.AddDapr();
+var options = new DaprSidecarOptions
+{
+    ResourcesPaths = ImmutableHashSet.Create(Directory.GetCurrentDirectory() + "/../../../../dapr")
+};
 
 // Add our API project with Dapr sidecar
 var api = builder.AddProject<Projects.Wigo4it_Chat_Api>("wigo4it-chat-api")
-    .WithDaprSidecar();
+    .WithDaprSidecar(options);
 
 builder.Build().Run();
